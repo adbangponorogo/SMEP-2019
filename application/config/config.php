21ccc583906d2de34f358b77ad62bd26a300fce1
Danang Ponorogo <danang.dandies@gmail.com>
2019-01-01 15:58:22 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -25,10 +25,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $base_url = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https' : 'http');
 $base_url .= '://' . $_SERVER['HTTP_HOST'];
-// $base_url .= str_replace(basename($_SERVER['SCRIPT_NAME']),'',$_SERVER['SCRIPT_NAME']);
-$base_url .= '/smep_2019/';
+$base_url .= str_replace(basename($_SERVER['SCRIPT_NAME']),'',$_SERVER['SCRIPT_NAME']);
 $config['base_url'] = $base_url;
-// $config['base_url'] = 'http://lpse.or.id/smep_2019/';
 
 /*
 |--------------------------------------------------------------------------
