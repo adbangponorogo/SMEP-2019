d7ef3d69dedd63f86945dc1f9614e1c954c65a0a
LPSE Kabupaten Ponorogo <45382893+adbangponorogo@users.noreply.github.com>
2019-01-02 16:20:55 +0700

    mengembalikan ke awal

--- application/config/database.php
+++ application/config/database.php
@@ -47,7 +47,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |				'ssl_key'    - Path to the private key file
 |				'ssl_cert'   - Path to the public key certificate file
 |				'ssl_ca'     - Path to the certificate authority file
-|				'ssl_capath' - Path to a directory containing trusted CA certificats in PEM format
+|				'ssl_capath' - Path to a directory containing trusted CA certificates in PEM format
 |				'ssl_cipher' - List of *allowed* ciphers to be used for the encryption, separated by colons (':')
 |				'ssl_verify' - TRUE/FALSE; Whether verify the server certificate or not ('mysqli' only)
 |
@@ -73,7 +73,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-if ($_SERVER['SERVER_NAME'] == "localhost") {
+if ($_SERVER["SERVER_NAME"] == "localhost") {
 	$db['default'] = array(
 		'dsn'	=> '',
 		'hostname' => 'localhost',
@@ -117,5 +117,5 @@ else{
 		'stricton' => FALSE,
 		'failover' => array(),
 		'save_queries' => TRUE
-	);	
+	);
 }
