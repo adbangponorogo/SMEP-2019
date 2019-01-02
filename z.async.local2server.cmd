0898cd7698e2ee74fd7340271ae8231578ff70d6
Danang Ponorogo <danang.dandies@gmail.com>
2019-01-02 19:16:49 +0700

    Update z.async.local2server.cmd

--- z.async.local2server.cmd
+++ z.async.local2server.cmd
@@ -4,7 +4,6 @@ SET CWRSYNCHOME=%PROGRAMFILES%\cwRsync\bin
 SET CWOLDPATH=%PATH%
 SET PATH=%CWRSYNCHOME%;%PATH%
 
-rem rsync -avp --chmod=u=rwx,go=rx --delete-before --ignore-errors --force lpsepono@lpse.or.id:/home/lpsepono/www/smep_2019/ /cygdrive/c/wamp64/www/smep19old/
-rsync -avp --chmod=u=rwx,go=rx --delete-before --ignore-errors --force /cygdrive/c/wamp64/www/smep19old/ /cygdrive/c/wamp64/www/smep19nu/
+rsync -avp --chmod=u=rwx,go=rx --delete-before --ignore-errors --force /cygdrive/c/wamp64/www/smep19/ lpsepono@lpse.or.id:/home/lpsepono/www/smep_2019/
 
 pause
