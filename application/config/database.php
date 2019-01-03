f1193524ad6f94a8feddc2703297b9a9044117c4
LPSE Kabupaten Ponorogo <45382893+adbangponorogo@users.noreply.github.com>
2019-01-03 08:02:01 +0700

    Upload ulang folder system - errornya balik lagi

--- application/config/database.php
+++ application/config/database.php
@@ -73,30 +73,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-if ($_SERVER["SERVER_NAME"] == "localhost") {
-	$db['default'] = array(
-		'dsn'	=> '',
-		'hostname' => 'localhost',
-		'username' => 'root',
-		'password' => 'Bismillah123',
-		'database' => 'db_smep_2019',
-		'dbdriver' => 'mysqli',
-		'dbprefix' => '',
-		'pconnect' => FALSE,
-		'db_debug' => (ENVIRONMENT !== 'production'),
-		'cache_on' => FALSE,
-		'cachedir' => '',
-		'char_set' => 'utf8',
-		'dbcollat' => 'utf8_general_ci',
-		'swap_pre' => '',
-		'encrypt' => FALSE,
-		'compress' => FALSE,
-		'stricton' => FALSE,
-		'failover' => array(),
-		'save_queries' => TRUE
-	);
-}
-else{
 	$db['default'] = array(
 		'dsn'	=> '',
 		'hostname' => 'localhost',
@@ -118,4 +94,8 @@ else{
 		'failover' => array(),
 		'save_queries' => TRUE
 	);
-}
+
+// if ($_SERVER["SERVER_NAME"] == "localhost") {
+// }
+// else{	
+// }
