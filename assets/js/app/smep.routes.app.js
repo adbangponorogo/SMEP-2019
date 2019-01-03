a14d54ac9626a2ee77ba7ca61735635959db3a82
LPSE Kabupaten Ponorogo <45382893+adbangponorogo@users.noreply.github.com>
2019-01-03 16:54:49 +0700

    Instan kategori program-kategori-ro
    revisi karena yang sebelumnya ada kesalahan logika

--- assets/js/app/smep.routes.app.js
+++ assets/js/app/smep.routes.app.js
@@ -10,11 +10,6 @@ jQuery(document).ready(function(){
 
 	// --------------- Application --------------- //
 	jQuery(document).on("click", ".smep-app-dashboard-page", function(){
-		jQuery(".smep-unique-token").val("");
-		jQuery(".smep-unique-edit-token").val("");
-		jQuery(".smep-idprogram-token").val("");
-		jQuery(".smep-idkegiatan-token").val("");
-		jQuery(".smep-idro-token").val("");
 		jQuery(".smep-content-page").load("app/dashboard/main-page");
 		return false;
 	});
@@ -22,71 +17,36 @@ jQuery(document).ready(function(){
 
 	// --------------- Data Umum --------------- //
 	jQuery(document).on("click", ".smep-datum-penja-page", function(){
-		jQuery(".smep-unique-token").val("");
-		jQuery(".smep-unique-edit-token").val("");
-		jQuery(".smep-idprogram-token").val("");
-		jQuery(".smep-idkegiatan-token").val("");
-		jQuery(".smep-idro-token").val("");
 		jQuery(".smep-content-page").load("data-umum/penanggung-jawab/main-page");
 		return false;
 	});
 
 	jQuery(document).on("click", ".smep-datum-daftan-page", function(){
-		jQuery(".smep-unique-token").val("");
-		jQuery(".smep-unique-edit-token").val("");
-		jQuery(".smep-idprogram-token").val("");
-		jQuery(".smep-idkegiatan-token").val("");
-		jQuery(".smep-idro-token").val("");
 		jQuery(".smep-content-page").load("data-umum/kegiatan/main-page");
 		return false;
 	});
 
 	jQuery(document).on("click", ".smep-datum-dator-page", function(){
-		jQuery(".smep-unique-token").val("");
-		jQuery(".smep-unique-edit-token").val("");
-		jQuery(".smep-idprogram-token").val("");
-		jQuery(".smep-idkegiatan-token").val("");
-		jQuery(".smep-idro-token").val("");
 		jQuery(".smep-content-page").load("data-umum/data-organisasi/main-page");
 		return false;
 	});
 
 	jQuery(document).on("click", ".smep-datum-patan-page", function(){
-		jQuery(".smep-unique-token").val("");
-		jQuery(".smep-unique-edit-token").val("");
-		jQuery(".smep-idprogram-token").val("");
-		jQuery(".smep-idkegiatan-token").val("");
-		jQuery(".smep-idro-token").val("");
 		jQuery(".smep-content-page").load("data-umum/pagu-kegiatan/main-page");
 		return false;
 	});
 
 	jQuery(document).on("click", ".smep-datum-dapan-page", function(){
-		jQuery(".smep-unique-token").val("");
-		jQuery(".smep-unique-edit-token").val("");
-		jQuery(".smep-idprogram-token").val("");
-		jQuery(".smep-idkegiatan-token").val("");
-		jQuery(".smep-idro-token").val("");
 		jQuery(".smep-content-page").load("data-umum/pencairan-sppd/main-page");
 		return false;
 	});
 
 	jQuery(document).on("click", ".smep-datum-tangan-page", function(){
-		jQuery(".smep-unique-token").val("");
-		jQuery(".smep-unique-edit-token").val("");
-		jQuery(".smep-idprogram-token").val("");
-		jQuery(".smep-idkegiatan-token").val("");
-		jQuery(".smep-idro-token").val("");
 		jQuery(".smep-content-page").load("data-umum/target-keuangan/main-page");
 		return false;
 	});
 
 	jQuery(document).on("click", ".smep-datum-tasik-page", function(){
-		jQuery(".smep-unique-token").val("");
-		jQuery(".smep-unique-edit-token").val("");
-		jQuery(".smep-idprogram-token").val("");
-		jQuery(".smep-idkegiatan-token").val("");
-		jQuery(".smep-idro-token").val("");
 		jQuery(".smep-content-page").load("data-umum/target-fisik/main-page");
 		return false;
 	});
@@ -94,31 +54,16 @@ jQuery(document).ready(function(){
 
 	// --------------- Entry Data --------------- //
 	jQuery(document).on("click", ".smep-enda-rup-page", function(){
-		jQuery(".smep-unique-token").val("");
-		jQuery(".smep-unique-edit-token").val("");
-		jQuery(".smep-idprogram-token").val("");
-		jQuery(".smep-idkegiatan-token").val("");
-		jQuery(".smep-idro-token").val("");
 		jQuery(".smep-content-page").load("entry-data/data-rup/main-page");
 		return false;
 	});
 	
 	jQuery(document).on("click", ".smep-enda-sirup-page", function(){
-		jQuery(".smep-unique-token").val("");
-		jQuery(".smep-unique-edit-token").val("");
-		jQuery(".smep-idprogram-token").val("");
-		jQuery(".smep-idkegiatan-token").val("");
-		jQuery(".smep-idro-token").val("");
 		jQuery(".smep-content-page").load("entry-data/realisasi-rup/main-page");
 		return false;
 	});
 
 	jQuery(document).on("click", ".smep-enda-sitepra-page", function(){
-		jQuery(".smep-unique-token").val("");
-		jQuery(".smep-unique-edit-token").val("");
-		jQuery(".smep-idprogram-token").val("");
-		jQuery(".smep-idkegiatan-token").val("");
-		jQuery(".smep-idro-token").val("");
 		jQuery(".smep-content-page").load("entry-data/realisasi-tepra/main-page");
 		return false;
 	});
@@ -126,71 +71,36 @@ jQuery(document).ready(function(){
 
 	// --------------- Laporan --------------- //
 	jQuery(document).on("click", ".smep-lapor-rapan-page", function(){
-		jQuery(".smep-unique-token").val("");
-		jQuery(".smep-unique-edit-token").val("");
-		jQuery(".smep-idprogram-token").val("");
-		jQuery(".smep-idkegiatan-token").val("");
-		jQuery(".smep-idro-token").val("");
 		jQuery(".smep-content-page").load("laporan/rencana-pengadaan/main-page");
 		return false;
 	});
 
 	jQuery(document).on("click", ".smep-lapor-tepra-perencanaan-page", function(){
-		jQuery(".smep-unique-token").val("");
-		jQuery(".smep-unique-edit-token").val("");
-		jQuery(".smep-idprogram-token").val("");
-		jQuery(".smep-idkegiatan-token").val("");
-		jQuery(".smep-idro-token").val("");
 		jQuery(".smep-content-page").load("laporan/tepra/perencanaan/main-page");
 		return false;
 	});
 
 	jQuery(document).on("click", ".smep-lapor-tepra-realisasi-page", function(){
-		jQuery(".smep-unique-token").val("");
-		jQuery(".smep-unique-edit-token").val("");
-		jQuery(".smep-idprogram-token").val("");
-		jQuery(".smep-idkegiatan-token").val("");
-		jQuery(".smep-idro-token").val("");
 		jQuery(".smep-content-page").load("laporan/tepra/realisasi/main-page");
 		return false;
 	});
 
 	jQuery(document).on("click", ".smep-lapor-lapan-page", function(){
-		jQuery(".smep-unique-token").val("");
-		jQuery(".smep-unique-edit-token").val("");
-		jQuery(".smep-idprogram-token").val("");
-		jQuery(".smep-idkegiatan-token").val("");
-		jQuery(".smep-idro-token").val("");
 		jQuery(".smep-content-page").load("laporan/laporan-pengadaan/main-page");
 		return false;
 	});
 
 	jQuery(document).on("click", ".smep-lapor-rebela-page", function(){
-		jQuery(".smep-unique-token").val("");
-		jQuery(".smep-unique-edit-token").val("");
-		jQuery(".smep-idprogram-token").val("");
-		jQuery(".smep-idkegiatan-token").val("");
-		jQuery(".smep-idro-token").val("");
 		jQuery(".smep-content-page").load("laporan/realisasi-belanja-langsung/main-page");
 		return false;
 	});
 
 	jQuery(document).on("click", ".smep-lapor-bast-page", function(){
-		jQuery(".smep-unique-token").val("");
-		jQuery(".smep-unique-edit-token").val("");
-		jQuery(".smep-idprogram-token").val("");
-		jQuery(".smep-idkegiatan-token").val("");
-		jQuery(".smep-idro-token").val("");
 		jQuery(".smep-content-page").load("laporan/bast/main-page");
 		return false;
 	});
 
 	jQuery(document).on("click", ".smep-lapor-rup-page", function(){
-		jQuery(".smep-unique-token").val("");
-		jQuery(".smep-unique-edit-token").val("");
-		jQuery(".smep-idprogram-token").val("");
-		jQuery(".smep-idkegiatan-token").val("");
-		jQuery(".smep-idro-token").val("");
 		jQuery(".smep-content-page").load("laporan/rup/main-page");
 		return false;
 	});
@@ -200,72 +110,49 @@ jQuery(document).ready(function(){
 
 	// --------------- Administrator --------------- //
 	jQuery(document).on("click", ".smep-admin-pengsi-page", function(){
-		jQuery(".smep-unique-token").val("");
-		jQuery(".smep-unique-edit-token").val("");
-		jQuery(".smep-idprogram-token").val("");
-		jQuery(".smep-idkegiatan-token").val("");
-		jQuery(".smep-idro-token").val("");
 		jQuery(".smep-content-page").load("administrator/pengguna-aplikasi/main-page");
 		return false;
 	});
 
 	jQuery(document).on("click", ".smep-admin-rup-page", function(){
-		jQuery(".smep-unique-token").val("");
-		jQuery(".smep-unique-edit-token").val("");
-		jQuery(".smep-idprogram-token").val("");
-		jQuery(".smep-idkegiatan-token").val("");
-		jQuery(".smep-idro-token").val("");
 		jQuery(".smep-content-page").load("administrator/rup/main-page");
 		return false;
 	});
 
 	jQuery(document).on("click", ".smep-admin-rapan-page", function(){
-		jQuery(".smep-unique-token").val("");
-		jQuery(".smep-unique-edit-token").val("");
-		jQuery(".smep-idprogram-token").val("");
-		jQuery(".smep-idkegiatan-token").val("");
-		jQuery(".smep-idro-token").val("");
 		jQuery(".smep-content-page").load("administrator/rencana-pengadaan/main-page");
 		return false;
 	});
 
 	jQuery(document).on("click", ".smep-admin-lapan-page", function(){
-		jQuery(".smep-unique-token").val("");
-		jQuery(".smep-unique-edit-token").val("");
-		jQuery(".smep-idprogram-token").val("");
-		jQuery(".smep-idkegiatan-token").val("");
-		jQuery(".smep-idro-token").val("");
 		jQuery(".smep-content-page").load("administrator/laporan-pengadaan/main-page");
 		return false;
 	});
 
 	jQuery(document).on("click", ".smep-admin-rebela-page", function(){
-		jQuery(".smep-unique-token").val("");
-		jQuery(".smep-unique-edit-token").val("");
-		jQuery(".smep-idprogram-token").val("");
-		jQuery(".smep-idkegiatan-token").val("");
-		jQuery(".smep-idro-token").val("");
 		jQuery(".smep-content-page").load("administrator/realisasi-belanja-langsung/main-page");
 		return false;
 	});
 
 	jQuery(document).on("click", ".smep-admin-misc-page", function(){
-		jQuery(".smep-unique-token").val("");
-		jQuery(".smep-unique-edit-token").val("");
-		jQuery(".smep-idprogram-token").val("");
-		jQuery(".smep-idkegiatan-token").val("");
-		jQuery(".smep-idro-token").val("");
 		jQuery(".smep-content-page").load("administrator/misc/main-page");
 		return false;
 	});
 
 	jQuery(document).on("click", ".smep-admin-rup-aktual-page", function(){
+		jQuery(".smep-content-page").load("administrator/rup-aktual/main-page");
+		return false;
+	});
+
+
+	jQuery(document).on("click", ".empty-value", function(){
 		jQuery(".smep-unique-token").val("");
 		jQuery(".smep-unique-edit-token").val("");
 		jQuery(".smep-idprogram-token").val("");
 		jQuery(".smep-idkegiatan-token").val("");
 		jQuery(".smep-idro-token").val("");
-		jQuery(".smep-content-page").load("administrator/rup-aktual/main-page");
 		return false;
 	});
+
+
 });
